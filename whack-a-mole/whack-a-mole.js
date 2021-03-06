1ef35bc9b5357cdbe9ea14f5aec7409797302f81
Fliss Roberts <hello.greenback@gmail.com>
2021-03-06 17:51:59 +1300

    try to add onclick like tutorial

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -2,4 +2,28 @@
  // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+//get a random cell - tick
+// mole img appears in random cell
+// click on mole
+// mole generates in new random cell
+
+var cells = document.getElementsByTagName("TD")
+
+var randomIndex = Math.floor(Math.random() * cells.length);
+
+var randomCell = cells[randomIndex]
+
+var mole = document.createElement('img');
+mole.src = './mole.png';
+mole.id = 'mole'
+
+randomCell.appendChild(mole);
+
+mole onclick =  whackDaMole;
+
+function whackDaMole {
+  console.log("Whack da Mole")
+}
+
+
+console.log(randomCell)

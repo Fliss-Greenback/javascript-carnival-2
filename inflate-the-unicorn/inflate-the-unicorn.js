a37863d94273e62182eab86b11fde048a8798a60
Fliss Roberts <hello.greenback@gmail.com>
2021-03-06 16:10:18 +1300

    completed following J Quested's walkthrough - my 3rd unicorn is flaccid and I don't know why!

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,23 @@
   // -    -   -   -   -  //
  // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
+document.getElementById("uni1").onclick = changeImage
+document.getElementById("uni2").onclick = changeImage
+document.getElementById("uni3").onclick = changeImage
 
-console.log("Inflate The Unicorn!")
+var inflation = [0,0,0]
+
+
+function changeImage(event) {
+  var unicorn = event.target
+  var id = unicorn.id[3]
+
+  inflation[id]++
+
+    if (inflation[id] == 4)
+        inflation[id] = 0
+        
+      unicorn.src = "./images/unicorn-" + inflation[id] + ".png"
+    
+}
+
